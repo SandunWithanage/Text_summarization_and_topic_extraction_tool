abb1ec403341f619466d282e39e1b955b49c68f1
hasheni04 <133528167+hasheni04@users.noreply.github.com>
2025-05-03 23:02:11 +0530

    functions

--- summarizer.py
+++ summarizer.py
@@ -1,4 +1,12 @@
 #
 from transformers import pipeline
 # Load summarizer once 
-summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")# Load summarizer once (e.g., outside functions if using Streamlit)
+#funtion
+
+def summarize_text(text, max_length=130, min_length=30):
+    # Truncate to avoid IndexError (BART = 1024 tokens, roughly ~1024*0.75 characters)
+    if len(text) > 3500:
+        text = text[:3500] 
+
+
