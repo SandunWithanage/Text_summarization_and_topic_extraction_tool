dd2da290ca4ff362625bd475e78876806abe85ff
hasheni04 <133528167+hasheni04@users.noreply.github.com>
2025-05-03 23:04:21 +0530

    truncate the text

--- summarizer.py
+++ summarizer.py
@@ -8,5 +8,9 @@ def summarize_text(text, max_length=130, min_length=30):
     # Truncate to avoid IndexError (BART = 1024 tokens, roughly ~1024*0.75 characters)
     if len(text) > 3500:
         text = text[:3500] 
+        def summarize_text(text, max_length=130, min_length=30):
+    # Truncate to avoid IndexError (BART = 1024 tokens, roughly ~1024*0.75 characters)
+    if len(text) > 3500:
+        text = text[:3500]
 
 
